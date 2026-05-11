329b0639c73dceb7a0964f6516da201008a98ca3
megan <megan-tsz-yau_pang28@milton.edu>
2026-05-11 17:53:29 -0400

    is Flipped for dealer

--- src/Card.java
+++ src/Card.java
@@ -2,14 +2,27 @@ public class Card {
     String rank;
     String suit;
     int value;
+    boolean isFlipped;
 
     public Card(String prank, String psuit, int pvalue) {
         rank = prank;
         suit = psuit;
         value = pvalue;
+        isFlipped = false;
     }
+    public void flipCard() {
+        isFlipped = !isFlipped;
+    }
+
 
     public String toString() {
         return rank + " of " + suit;
+    }
+    public void printCard() {
+        if (isFlipped) {
+            System.out.println("[Hidden Card]");
+        } else {
+            System.out.println(value + " of " + suit);
+        }
     }
 }
